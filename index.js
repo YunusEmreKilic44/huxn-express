be0b34378888c784b17e4608f039a3053f40fec3
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-02-05 09:51:04 +0300

    Basic get routing added. nodemon package installed

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-import express from "express";
+// Create Folder
+// npm init -y
+// npm i express
+// Create Instance express
+// Provide Port :8000
+// Basic Route
 
+import express from "express";
 const app = express();
 
+// Basic Routing
+
 app.get("/", (req, res) => {
-  res.send("Welcome to Express.js");
+  res.send("<h1>Welcome To Home</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About </h1>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact Page</h1>");
 });
 
 app.listen(8000, () => {
