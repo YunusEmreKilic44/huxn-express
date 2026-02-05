fd6ff13b2f4492b501c8a6f90c019acc99907987
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-02-05 12:22:44 +0300

    Query String

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from "express";
-import student from "./routes/student.js";
 
 const app = express();
 
-app.use("/student", student);
+// Query String ? &
+
+app.get("/product", (req, res) => {
+  const { category, id } = req.query;
+
+  res.send(`Product Category: ${category} & Product ID: ${id}`);
+});
 
 app.listen(8000, () => {
   console.log(`Server running on port 8000`);
